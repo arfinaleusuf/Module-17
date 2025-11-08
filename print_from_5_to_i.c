dd91730f46e35b4fc46ac1ae3c8975feb49a0733
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-08 23:26:26 +0600

    printing 5 to 1 with recursion

---
+++ print_from_5_to_i.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void hello(int i)
+{
+    if(i == 0) //end condition
+    {
+        return;
+    }
+    printf("%d\n",i);
+    hello(i-1); // incriment and decriment 
+}
+int main()
+{
+    int i = 5;
+    hello(i);  // initialization/starting
+    return 0;
+}
