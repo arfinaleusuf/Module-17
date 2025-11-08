10cb4ab8e14dce460c5950584ecdc52c1dd4f205
ArfinAlEusuf <arfinaleusuf@gmail.com>
2025-11-08 23:43:37 +0600

    controal recursion

--- print_from_5_to_i.c
+++ print_from_5_to_i.c
@@ -1,16 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 void hello(int i)
 {
-    if(i == 0) //end condition
+    if (i == 0) // end condition
     {
         return;
     }
-    printf("%d\n",i);
-    hello(i-1); // incriment and decriment 
+    if (i % 2 == 0)
+    {
+        printf("%d\n", i);
+    }
+    hello(i - 1); // incriment and decriment
 }
 int main()
 {
     int i = 5;
-    hello(i);  // initialization/starting
+    hello(i); // initialization/starting
     return 0;
 }
